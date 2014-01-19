68101b3753cec175d06713fd56e221eb50a2104a
Meh <Meh@Meh-PC.lan>
2014-01-19 14:16:53 +0200

    featur 300

---
+++ src/testing1234/Feature300.java
@@ -0,0 +1,9 @@
+package testing1234;
+
+public class Feature300 {
+	
+	public Feature300(){
+		System.out.println("Constructor of feature 300");
+	}
+
+}
--- src/testing1234/Main.java
+++ src/testing1234/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		Feature300 f = new Feature300();
 
 	}
 
